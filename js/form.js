54c0546288e8e1b10ad094129faa2e41f6969fad
Stepan Vasilyev <intellectualspecialist@gmail.com>
2024-03-22 19:43:42 +0500

    Добавляет логику редактирования фото

--- js/form.js
+++ js/form.js
@@ -1,5 +1,6 @@
 import {isEscapeKey, openModalElement, closeModalElement} from './util.js';
 import {onFormSubmit} from './validate-form.js';
+import {onButtonSmallerClick, onButtonBiggerClick, onFilterChange} from './edit-photo';
 
 const downloadForm = document.querySelector('.img-upload__form');
 const modalForm = downloadForm.querySelector('.img-upload__overlay');
@@ -7,7 +8,10 @@ const fileDownloadControl = downloadForm.querySelector('.img-upload__input');
 const formCloseButton = modalForm.querySelector('.img-upload__cancel');
 const inputHashtag = modalForm.querySelector('.text__hashtags');
 const commentField = modalForm.querySelector('.text__description');
-
+const buttonSmaller = modalForm.querySelector('.scale__control--smaller');
+const buttonBigger = modalForm.querySelector('.scale__control--bigger');
+const sliderContainer = modalForm.querySelector('.img-upload__effect-level');
+const filters = modalForm.querySelectorAll('[name="effect"]');
 
 const onFileDownloadControlChange = () => {
   openModalForm();
@@ -38,8 +42,15 @@ function openModalForm() {
   openModalElement(modalForm);
 
   downloadForm.addEventListener('submit', onFormSubmit);
+  filters.forEach((filter) => {
+    filter.addEventListener('change', onFilterChange);
+  });
   formCloseButton.addEventListener('click', onCloseModalButtonClick);
   document.addEventListener('keydown', onDocumentKeydown);
+  buttonSmaller.addEventListener('click', onButtonSmallerClick);
+  buttonBigger.addEventListener('click', onButtonBiggerClick);
+
+  sliderContainer.classList.add('hidden');
 
   inputHashtag.addEventListener('focus', onFieldFocus);
   commentField.addEventListener('focus', onFieldFocus);
@@ -49,8 +60,15 @@ function closeModalForm() {
   closeModalElement(modalForm);
 
   downloadForm.removeEventListener('submit', onFormSubmit);
+  filters.forEach((filter) => {
+    filter.removeEventListener('change', onFilterChange);
+  });
   formCloseButton.removeEventListener('click', onCloseModalButtonClick);
   document.removeEventListener('keydown', onDocumentKeydown);
+  buttonSmaller.removeEventListener('click', onButtonSmallerClick);
+  buttonBigger.removeEventListener('click', onButtonBiggerClick);
+
+  sliderContainer.classList.remove('hidden');
 
   fileDownloadControl.value = '';
 
